4f62d5029da83970360663fe491abac83758e720
caatty <caattylife@gmail.com>
2023-05-03 12:16:06 +0600

    updated1 app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Hello from Caatty SE team onnorokom"
+    return "Hello from Caatty SE team onnorokom pathshala"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
